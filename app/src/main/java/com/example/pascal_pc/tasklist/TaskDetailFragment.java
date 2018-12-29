e8ee2bc78adad0da76f7857653e12245bac51b6c
Pascal-PC <alisalek23@gmail.com>
2018-12-29 17:26:12 +0330

    Create ViewPager and tab Layout and link them

--- app/src/main/java/com/example/pascal_pc/tasklist/TaskDetailFragment.java
+++ app/src/main/java/com/example/pascal_pc/tasklist/TaskDetailFragment.java
@@ -2,6 +2,7 @@ package com.example.pascal_pc.tasklist;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +10,10 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.pascal_pc.tasklist.models.Task;
+import com.example.pascal_pc.tasklist.models.TaskList;
+
+import java.text.SimpleDateFormat;
+import java.util.UUID;
 
 
 /**
@@ -16,15 +21,17 @@ import com.example.pascal_pc.tasklist.models.Task;
  */
 public class TaskDetailFragment extends Fragment {
 
+    private static final String EXRTA_TASK_ID = "com.example.pascal_pc.tasklist.taskId";
     private TextView mDescriptionTxtView;
     private TextView mTimeTxtView;
     private TextView mDateTxtView;
     private Task mTask;
 
-    public static TaskDetailFragment newInstance() {
+    public static TaskDetailFragment newInstance(UUID taskID) {
 
         Bundle args = new Bundle();
 
+        args.putSerializable(EXRTA_TASK_ID,taskID);
         TaskDetailFragment fragment = new TaskDetailFragment();
         fragment.setArguments(args);
         return fragment;
@@ -33,6 +40,13 @@ public class TaskDetailFragment extends Fragment {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        UUID taskId= (UUID) getArguments().getSerializable(EXRTA_TASK_ID);
+        mTask=TaskList.getInstance().getCrime(taskId);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -42,6 +56,10 @@ public class TaskDetailFragment extends Fragment {
         mDescriptionTxtView=view.findViewById(R.id.description_text_view);
         mDateTxtView=view.findViewById(R.id.date_text_view);
         mTimeTxtView=view.findViewById(R.id.time_text_view);
+
+        mDescriptionTxtView.setText(mTask.getDescription());
+        mDateTxtView.setText(new SimpleDateFormat("EEE-d MMM-yyyy ").format(mTask.getDate().toString()));
+        mTimeTxtView.setText(new SimpleDateFormat("kk:mm").format(mTask.getDate().toString()));
 
         return view;
     }
--- app/src/main/java/com/example/pascal_pc/tasklist/TaskPagerActivity.java
+++ app/src/main/java/com/example/pascal_pc/tasklist/TaskPagerActivity.java
@@ -1,13 +1,55 @@
 package com.example.pascal_pc.tasklist;
 
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class TaskPagerActivity extends AppCompatActivity {
 
+    private ViewPager mViewPager;
+    private TabLayout mTaskTabLayout;
+    private String[] mTitle=new String[]{"All","DONE","UNDONE"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_task_pager);
+
+        mViewPager=findViewById(R.id.task_view_pager);
+        mTaskTabLayout=findViewById(R.id.tablayout_task_pager);
+
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int i) {
+                switch (i){
+                    case 0:
+                    return null;
+
+                    case 1:
+                        return null;
+
+                    case 2:
+                        return null;
+                }
+                return null;
+            }
+
+            @Nullable
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return mTitle[position];
+            }
+
+            @Override
+            public int getCount() {
+                return 3;
+            }
+        });
+        mTaskTabLayout.setupWithViewPager(mViewPager);
+
     }
 }
