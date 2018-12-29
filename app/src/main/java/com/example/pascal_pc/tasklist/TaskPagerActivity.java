2ec04b3d22cbb01634a2c9407eeda7926c20acdf
Pascal-PC <alisalek23@gmail.com>
2018-12-29 15:36:23 +0330

    creat task's model

--- app/src/main/java/com/example/pascal_pc/tasklist/TaskPagerActivity.java
+++ app/src/main/java/com/example/pascal_pc/tasklist/TaskPagerActivity.java
@@ -3,7 +3,7 @@ package com.example.pascal_pc.tasklist;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class TaskPagerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
