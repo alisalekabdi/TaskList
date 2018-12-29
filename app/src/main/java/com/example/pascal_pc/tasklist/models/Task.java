187456b0601d48440895afaf19ccd1b6e4a92e6e
Pascal-PC <alisalek23@gmail.com>
2018-12-29 15:21:18 +0330

    creat task's model

---
+++ app/src/main/java/com/example/pascal_pc/tasklist/models/Task.java
@@ -0,0 +1,54 @@
+package com.example.pascal_pc.tasklist.models;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Task {
+
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private String mDescription;
+    private boolean mDone;
+
+    public Task(String title) {
+        mId =UUID.randomUUID();
+        mTitle=title;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public void setDescription(String description) {
+        mDescription = description;
+    }
+
+    public boolean isDone() {
+        return mDone;
+    }
+
+    public void setDone(boolean done) {
+        mDone = done;
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+}
---
+++ app/src/main/java/com/example/pascal_pc/tasklist/models/TaskList.java
@@ -0,0 +1,5 @@
+package com.example.pascal_pc.tasklist.models;
+
+public class TaskList {
+
+}
