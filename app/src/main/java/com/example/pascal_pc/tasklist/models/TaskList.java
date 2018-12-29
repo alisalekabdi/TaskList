b5d4445bd2801bc6452851cc041e116252870aa7
Pascal-PC <alisalek23@gmail.com>
2018-12-29 15:34:44 +0330

    creat task's model

--- app/src/main/java/com/example/pascal_pc/tasklist/models/TaskList.java
+++ app/src/main/java/com/example/pascal_pc/tasklist/models/TaskList.java
@@ -1,5 +1,34 @@
 package com.example.pascal_pc.tasklist.models;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.UUID;
+
 public class TaskList {
+
+    private static TaskList instance;
+    private LinkedHashMap<UUID, Task> mTasks;
+
+    private TaskList(){
+        mTasks=new LinkedHashMap<>();
+    }
+
+    public void addCrime(Task task){
+        mTasks.put(task.getId(),task);
+    }
+    public void removeCrime(Task task){
+        mTasks.remove(task.getId());
+    }
+
+    public static TaskList getInstance() {
+        if (instance == null)
+            instance = new TaskList();
+
+        return instance;
+    }
+    public List<Task> getCrimes() {
+        return new ArrayList<>(mTasks.values());
+    }
 
 }
